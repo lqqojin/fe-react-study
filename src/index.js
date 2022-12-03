fb87e423397e73cb7c84ba3ae2af931a5a08cd0c
lqqojin <lqqojin@gmail.com>
2022-12-03 21:39:54 +0900

    [5.24] 클래스 컴포넌트

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import AppCard from './AppCard';
 // import AppTheme from './AppTheme';
-import AppProducts from './basic/AppProducts'; // 5.21~22 로딩, 에러 상태 추가
+// import AppProducts from './basic/AppProducts'; // 5.21~22 로딩, 에러 상태 추가
+import AppClass from './AppClass'; // 5.24 클래스 컴포넌트
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppProducts />
+    <AppClass />
   </React.StrictMode>,
 );
 
